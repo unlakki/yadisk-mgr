51f528f073029778e93c7b924775b3de2b575a7c
Ferisef <ferisefbns@gmail.com>
2020-01-10 15:14:07 +0300

    Add entry point

---
+++ src/index.ts
@@ -0,0 +1,16 @@
+import Instance, * as Declarations from './DiskInstance';
+import Manager from './DiskManager';
+import DiskMgrError from './errors/DiskManagerError';
+
+export default Manager;
+
+export declare const DiskInstance: typeof Instance;
+
+export declare const SortBy: typeof Declarations.SortBy;
+export declare const ResourceType: typeof Declarations.ResourceType;
+
+export declare const DiskManagerError: typeof DiskMgrError;
+
+export declare type Resource = Declarations.Resource;
+export declare type DiskStatus = Declarations.DiskStatus;
+export declare type DirListOptions = Declarations.DirListOptions;
