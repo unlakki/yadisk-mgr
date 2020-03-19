81887421fb4095fe47b76543f0b4d55abc9fc8d0
Ferisef <ferisefbns@gmail.com>
2020-03-20 01:25:00 +0300

    Update export

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,10 @@
-import Instance, * as Declarations from './DiskInstance';
-import Manager from './DiskManager';
-import DiskMgrError from './errors/DiskManagerError';
-
-export default Manager;
-
-export declare const DiskInstance: typeof Instance;
-
-export declare const SortBy: typeof Declarations.SortBy;
-export declare const ResourceType: typeof Declarations.ResourceType;
-
-export declare const DiskManagerError: typeof DiskMgrError;
-
-export declare type Resource = Declarations.Resource;
-export declare type DiskStatus = Declarations.DiskStatus;
-export declare type DirListOptions = Declarations.DirListOptions;
+export { default as DiskManagerError } from './errors/DiskManagerError';
+export {
+  default as DiskInstance,
+  SortBy,
+  ResourceType,
+  Resource,
+  DiskStatus,
+  DirListOptions,
+} from './DiskInstance';
+export { default } from './DiskManager';
