b0b68eaef173afa5337eb456de75625c9e3e6da9
Ferisef <ferisefbns@gmail.com>
2020-04-19 22:08:18 +0300

    Remove lib

--- lib/index.d.ts
+++
@@ -1,3 +0,0 @@
-export { default as DiskManagerError } from './errors/DiskManagerError';
-export { default as DiskInstance, SortBy, ResourceType, Resource, DiskStatus, DirListOptions, } from './DiskInstance';
-export { default } from './DiskManager';
--- lib/utils/bufferToStream.d.ts
+++
@@ -1,4 +0,0 @@
-/// <reference types="node" />
-import { Stream } from 'stream';
-declare const _default: (buffer: Buffer) => Stream;
-export default _default;
