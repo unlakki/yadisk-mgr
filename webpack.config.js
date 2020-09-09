de730c8ab091e9e086fd7c0ecfa9cbc09e65c5d4
Ferisef <ferisefbns@gmail.com>
2020-09-10 02:15:47 +0300

    Add sourcemaps if NODE_ENV !== production

--- webpack.config.js
+++ webpack.config.js
@@ -1,50 +1,42 @@
 const path = require('path');
-const webpackNodeExternals = require('webpack-node-externals');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const webpack = require('webpack');
+const webpackNodeExternals = require('webpack-node-externals');
+const {
+  CleanWebpackPlugin
+} = require('clean-webpack-plugin');
 const TerserPlugin = require('terser-webpack-plugin');
+
+const isProd = process.env.NODE_ENV === 'production';
 
 module.exports = {
   target: 'node',
   entry: './src/index.ts',
-  mode: 'production',
+  mode: isProd ? 'production' : 'development',
   output: {
     libraryTarget: 'umd',
-    path: path.resolve('./lib'),
+    path: path.resolve('lib'),
     filename: 'index.js',
   },
   module: {
-    rules: [
-      {
-        test: /\.ts$/,
-        use: {
-          loader: 'ts-loader',
-        },
+    rules: [{
+      test: /\.ts$/,
+      use: {
+        loader: 'ts-loader',
       },
-    ],
+    }, ],
   },
   resolve: {
     extensions: ['.ts', '.js'],
   },
   externals: [
     webpackNodeExternals({
-      whitelist: [
-        'bluebird',
-        'request',
-        'request-promise',
-        'request-promise/errors',
-      ],
+      whitelist: ['bluebird'],
     }),
   ],
-  devtool: false,
-  plugins: [
-    new CleanWebpackPlugin(),
-    new webpack.optimize.ModuleConcatenationPlugin(),
-  ],
+  devtool: isProd ? false : 'source-map',
+  plugins: [new CleanWebpackPlugin(), new webpack.optimize.ModuleConcatenationPlugin()],
   optimization: {
     minimize: true,
-    minimizer: [
-      new TerserPlugin(),
-    ],
+    minimizer: [new TerserPlugin()],
   },
 };
