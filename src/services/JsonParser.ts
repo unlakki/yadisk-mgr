c50c39b5c86560705366f77369a17cf1ee0e07fb
Ferisef <ferisefbns@gmail.com>
2020-09-21 20:15:28 +0300

    Update json parse error

--- src/services/JsonParser.ts
+++ src/services/JsonParser.ts
@@ -1,9 +1,16 @@
 import IJsonParser from './interfaces/IJsonParser';
+import DiskError from '../errors/DiskError';
 
 class JsonParser implements IJsonParser {
-  public parse = <T>(data: string) => <T>JSON.parse(data);
+  public parse = <T>(data: string) => {
+    try {
+      return <T>JSON.parse(data);
+    } catch (e) {
+      throw new DiskError('Error while parse json');
+    }
+  };
 
-  public tryParse = <T>(data: any) => {
+  public tryParse = <T>(data: string) => {
     try {
       return this.parse<T>(data);
     } catch (e) {
