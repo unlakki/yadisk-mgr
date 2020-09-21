ac325d0e3c3ea3ca827ad5127b316bd55cb09692
Ferisef <ferisefbns@gmail.com>
2020-09-21 20:17:06 +0300

    Updaet bad path part error

--- src/errors/BadPathPart.ts
+++ src/errors/BadPathPart.ts
@@ -1,5 +1,9 @@
-import BaseError from './BaseError';
+import DiskError from './DiskError';
 
-class BadPathPart extends BaseError {}
+class BadPathPart extends DiskError {
+  constructor(path: string) {
+    super(`Bad path part ${path}`);
+  }
+}
 
 export default BadPathPart;
