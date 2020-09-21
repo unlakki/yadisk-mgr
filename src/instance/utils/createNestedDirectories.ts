78f89d49fc4f610626087a7150fc68629a8e8fc0
Ferisef <ferisefbns@gmail.com>
2020-09-21 20:13:11 +0300

    Minor update

--- src/instance/utils/createNestedDirectories.ts
+++ src/instance/utils/createNestedDirectories.ts
@@ -1,42 +1,48 @@
 import { posix as Path } from 'path';
 import Bluebird from 'bluebird';
+import ResourceType from '../enums/ResourceType';
+import createDir from '../methods/createDir';
+import getResourceMetadata from '../methods/getResourceMetadata';
+import BadPathPart from '../../errors/BadPathPart';
+import DiskError from '../../errors/DiskError';
+import StringExtensions from '../../extensions/StringExtensions';
+import ErrorObject from '../../interfaces/ErrorObject';
 import IFetchProvider from '../../services/interfaces/IFetchProvider';
 import IJsonParser from '../../services/interfaces/IJsonParser';
-import ErrorObject from '../interfaces/ErrorObject';
-import removeLeadingChar from '../../extensions/removeLeadingChar';
-import getResourceMetadata from '../methods/getResourceMetadata';
-import createDir from '../methods/createDir';
-import ResourceType from '../enums/ResourceType';
-import BadPathPart from '../../errors/BadPathPart';
 
 const startPath = '/';
 
+const reducer = (fetchProvider: IFetchProvider, jsonParser: IJsonParser) => async (
+  createdPath: string,
+  currentDir: string,
+) => {
+  const currentPath = Path.join(createdPath, currentDir);
+
+  try {
+    const { type } = await getResourceMetadata(fetchProvider, jsonParser)(currentPath);
+    if (type !== ResourceType.Dir) {
+      throw new BadPathPart(currentDir);
+    }
+  } catch (e) {
+    if (e instanceof BadPathPart) {
+      throw e;
+    }
+
+    const err = jsonParser.tryParse<ErrorObject>(e.message);
+    if (err && !['DiskNotFoundError', 'DiskPathDoesntExistsError'].includes(err.error)) {
+      throw new DiskError(err.description);
+    }
+
+    await createDir(fetchProvider, jsonParser)(currentPath);
+  }
+
+  return currentPath;
+};
+
 const createNestedDirectories = (fetchProvider: IFetchProvider, jsonParser: IJsonParser, path: string) =>
   Bluebird.reduce<string, string>(
-    removeLeadingChar(path, Path.sep).split(Path.sep),
-    async (pathParts, pathPart) => {
-      const currentPath = Path.join(pathParts, pathPart);
-
-      try {
-        const metadata = await getResourceMetadata(fetchProvider, jsonParser)(currentPath);
-        if (metadata.type !== ResourceType.Dir) {
-          throw new BadPathPart('Path must only contain directories.');
-        }
-      } catch (e) {
-        if (e instanceof BadPathPart) {
-          throw e;
-        }
-
-        const err = jsonParser.parse<ErrorObject>(e.message);
-        if (!['DiskNotFoundError', 'DiskPathDoesntExistsError'].includes(err.error)) {
-          throw new Error(err.description);
-        }
-
-        await createDir(fetchProvider)(currentPath);
-      }
-
-      return currentPath;
-    },
+    StringExtensions.removeLeadingChar(path, Path.sep).split(Path.sep),
+    reducer(fetchProvider, jsonParser),
     startPath,
   );
 
